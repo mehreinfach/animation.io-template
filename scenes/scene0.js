9f61668cf09abe1242b3d81cc29194324c33b4db
Dushan Wegner <dushan@mehreinfach.de>
2013-11-02 11:21:11 +0100

    cleaning up

--- scenes/scene0.js
+++ scenes/scene0.js
@@ -1,19 +1,20 @@
-    var scene0 = function () {
+var scene0 = function () {
 
-    var sceneOptions = {
-      "animation": animation,
-      "id": "scene0",
-      "title": "Start",
-      "width": 1024,
-      "height": 768
-    };
+  var sceneOptions = {
+    "animation": animation,
+    "id": "scene0",
+    "title": "Start",
+    "width": 1024,
+    "height": 768
+  };
 
-    var scene = new Scene(sceneOptions);
+  var scene = new Scene(sceneOptions);
 
-    scene.scrollingPerspective(5, 1);
+  scene.scrollingPerspective(5, 1);
 
-    var bg_hinten = scene.createActor('szene0/bg0.jpg', 0, 0, 1024, 768).isInLayer(0);
-    
+  var bg_hinten = scene.createActor('szene0/bg0.jpg', 0, 0, 1024, 768).isInLayer(0);
 
-    return scene;
+  var gesicht = scene.createActor('bilder/mann.png', 100, 100).isInLayer(0);
+  
+  return scene;
 };
